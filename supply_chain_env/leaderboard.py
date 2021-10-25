45762357af0b973fa194072456c0a7d81fbf59c1
Sebastian Neubauer <sebastian.neubauer@jda.com>
2021-10-25 22:24:22 +0200

    Set temp submit url

--- supply_chain_env/leaderboard.py
+++ supply_chain_env/leaderboard.py
@@ -8,7 +8,7 @@ from requests.auth import HTTPBasicAuth
 
 
 LEADERBOARD_URL = (
-    "https://NAMEOFPROJECT.eu.live.external.byp.ai/add-user-score"
+    "https://leaderboard-brewyonder.eu.live.external.byp.ai/"
 )
 
 
