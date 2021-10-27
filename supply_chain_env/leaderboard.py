cf44ba67b6d2bf7b3c5c1adc73802f6d7cd23413
Fabian Gebhart <fabian.gebhart@gmail.com>
2021-10-27 20:06:48 +0200

    fix url

--- supply_chain_env/leaderboard.py
+++ supply_chain_env/leaderboard.py
@@ -8,7 +8,7 @@ from requests.auth import HTTPBasicAuth
 
 
 LEADERBOARD_URL = (
-    "https://leaderboard-brewyonder.eu.live.external.byp.ai/"
+    "https://leaderboard-brewyonder.eu.live.external.byp.ai/add-user-score"
 )
 
 
