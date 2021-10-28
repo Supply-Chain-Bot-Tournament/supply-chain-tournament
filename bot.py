7658b07e574edd160660af3078df97feda4ed5d6
Fabian Gebhart <fabian.gebhart@gmail.com>
2021-10-28 08:53:21 +0200

    improved values

--- bot.py
+++ bot.py
@@ -98,10 +98,10 @@ class Wholesaler:
         # TODO provide your implementation here:
 
         order_to_place = step_state["next_incoming_order"]
-        if step_state["current_stock"]+step_state["inbound_shipments"][0] < 15:
-            order_to_place += 5
-        if step_state["current_stock"]+step_state["inbound_shipments"][0] > 30:
-            order_to_place -= 5
+        if step_state["current_stock"]+step_state["inbound_shipments"][0] < 20:
+            order_to_place += 10
+        if step_state["current_stock"]+step_state["inbound_shipments"][0] > 20:
+            order_to_place -= 10
 
         return max(order_to_place, 0)
 
